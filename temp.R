90727ba2a8956d5726868004231e9e85ffde387c
AnmayG <84810366+AnmayG@users.noreply.github.com>
2021-06-18 15:44:33 -0500

    Delete temp.R

--- temp.R
+++
@@ -1,8 +0,0 @@
-library(DBI)
-library(RSQLite)
-download.file(url = "https://github.com/PPBDS/primer/blob/master/03-data/data/chinook.db?raw=true",
-              destfile = "temp.db", mode = "wb")
-con_chinook_data <- dbConnect(RSQLite::SQLite(), "temp.db")
-tables <- dbListTables(con_chinook_data)
-tables
-albums_db <- tbl(can_mov_db_con, "ratings")
